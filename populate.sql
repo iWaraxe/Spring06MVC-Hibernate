14d72c49f098f46842a2d7e89a5bbf4aaab288c2
iWaraxe <121664848+MariaESLIW@users.noreply.github.com>
2024-05-05 18:07:58 +0200

    Sql script for database population

---
+++ populate.sql
@@ -0,0 +1,22 @@
+INSERT INTO hibernate_db.employees (name, surname, department, salary, hero_name)
+VALUES
+    ('Tony', 'Stark', 'R&D', 10000, 'Iron Man'),
+    ('Steve', 'Rogers', 'Leadership', 8500, 'Captain America'),
+    ('Natasha', 'Romanoff', 'Security', 7500, 'Black Widow'),
+    ('Bruce', 'Banner', 'Science', 9500, 'Hulk'),
+    ('Peter', 'Parker', 'Photography', 4500, 'Spider-Man'),
+    ('Thor', 'Odinson', 'Public Relations', 9000, 'Thor'),
+    ('Carol', 'Danvers', 'Aerospace', 9200, 'Captain Marvel'),
+    ('Stephen', 'Strange', 'Medical', 9800, 'Doctor Strange'),
+    ('Wanda', 'Maximoff', 'Research', 8700, 'Scarlet Witch'),
+    ('Clint', 'Barton', 'Security', 7300, 'Hawkeye'),
+    ('Scott', 'Lang', 'Engineering', 4800, 'Ant-Man'),
+    ('Reed', 'Richards', 'Science', 11000, 'Mr. Fantastic'),
+    ('Hank', 'Pym', 'Science', 9500, 'Ant-Man'),
+    ('Nick', 'Fury', 'Security', 8500, NULL), -- No superhero name
+    ('Sam', 'Wilson', 'Leadership', 8200, 'Falcon'),
+    ('James', 'Rhodes', 'Aerospace', 8700, 'War Machine'),
+    ('Peggy', 'Carter', 'Diplomacy', 9000, NULL), -- No superhero name
+    ('Jane', 'Foster', 'Research', 8500, 'Mighty Thor'),
+    ('Hope', 'van Dyne', 'Engineering', 7600, 'The Wasp'),
+    ('Otto', 'Octavius', 'Medical', 9400, 'Doctor Octopus');
